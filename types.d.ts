8f4f66d226cd85b1ddacee3523bd69b5e2e980c6
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-03-12 11:26:56 -0500

    0.0.67

---
+++ types.d.ts
@@ -0,0 +1,24 @@
+import {SimpleWCInfo} from 'may-it-be/SimpleWCInfo';
+
+export interface XtalSideNaveProps {
+    /**
+     * indicates if side nav is (or should be) open or not.
+     */
+    open: boolean;    
+}
+
+/**
+ * Side nav (drawer) web component.
+ */
+export abstract class XtalSideNaveInfo implements SimpleWCInfo {
+    src: './xtal-side-nav.js';
+    tagName: 'xtal-side-nav';
+    props: XtalSideNaveProps;
+    cssParts: {
+        sideNav: 'drawer element that opens up',
+        opener: 'button that opens up the drawer',
+        closeBtn: 'button that closes the drawer',
+    }
+}
+
+export type Package = [XtalSideNaveInfo];
