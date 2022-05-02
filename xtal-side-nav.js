6b7d09a980ff6a8ac2a63ac1e0e0ca570098dc62
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-05-02 05:52:20 -0400

    0.0.82

--- xtal-side-nav.js
+++ xtal-side-nav.js
@@ -1,7 +1,9 @@
 import('be-definitive/be-definitive.js');
 import('be-active/be-active.js');
 
-document.body.insertAdjacentHTML('beforeend', `<template be-definitive='{"config":{"tagName":"xtal-side-nav","propDefaults":{"open":false,"transform":[{"buttonElements":[{"disabled":false},{"click":{"prop":"open","toggleProp":true}}]},{"asideElements":[{},{},{"data-open":"open","data-mode":"mode"}]}],"mode":"ltr"},"keyQueries":["[part=\\\"side-nav\\\"]"]}}'>
+if(!document.body){
+    document.addEventListener('DOMContentLoaded', () => {
+        document.body.insertAdjacentHTML('beforeend', `<template be-definitive='{"config":{"tagName":"xtal-side-nav","propDefaults":{"open":false,"transform":[{"buttonElements":[{"disabled":false},{"click":{"prop":"open","toggleProp":true}}]},{"asideElements":[{},{},{"data-open":"open","data-mode":"mode"}]}],"mode":"ltr"},"keyQueries":["[part=\\\"side-nav\\\"]"]}}'>
 <main part=main>
     <button disabled aria-label="Open Menu" part=opener class=opener>&#9776; <slot name=title></slot></button>
     <aside part=side-nav class=side-nav>
@@ -74,3 +76,79 @@ TODO:  use FLIP?
 } 
 </style>
 <be-hive></be-hive></template>`);
+    });
+}else{
+    document.body.insertAdjacentHTML('beforeend', `<template be-definitive='{"config":{"tagName":"xtal-side-nav","propDefaults":{"open":false,"transform":[{"buttonElements":[{"disabled":false},{"click":{"prop":"open","toggleProp":true}}]},{"asideElements":[{},{},{"data-open":"open","data-mode":"mode"}]}],"mode":"ltr"},"keyQueries":["[part=\\\"side-nav\\\"]"]}}'>
+<main part=main>
+    <button disabled aria-label="Open Menu" part=opener class=opener>&#9776; <slot name=title></slot></button>
+    <aside part=side-nav class=side-nav>
+        <button aria-label="Close Menu" part=close-btn>&times;</button>
+        <slot id="slot"></slot>
+    </aside>
+</main>
+
+<style be-adopted>
+:host {
+    display: block;
+    --drawer-width:250px;
+    --side-nav-position:fixed;
+}
+
+main {
+    position: relative;
+}
+
+.side-nav {
+    width: 0;
+    position: var(--side-nav-position);
+    display:flex;
+    flex-direction:column;
+    z-index: 10;
+    top: inherit;
+    left: inherit;
+    background-color: #111;
+    overflow-x: hidden;
+    transition: 0.5s;
+    padding-top: 60px;
+}
+.side-nav[data-mode="ltr"]{
+    left: 8px;
+}
+.side-nav[data-mode="rtl"]{
+    right: 8px;
+}
+/*
+TODO:  use FLIP?
+*/
+.side-nav[data-open]{
+    width: var(--drawer-width);
+}
+
+.side-nav:not([data-open]){
+    width: 0px;
+    height:0px;
+}
+
+.side-nav button {
+    position: absolute;
+    cursor:pointer;
+    top: 0;
+    right: 0px;
+    font-size: 36px;
+    margin-left: 50px;
+    transition: 0.3s;
+}
+.opener{
+    font-size:30px;
+    cursor:pointer
+}
+
+
+@media screen and (max-height: 450px) {
+    .side-nav {
+        padding-top: 15px;
+    }
+} 
+</style>
+<be-hive></be-hive></template>`);
+}
